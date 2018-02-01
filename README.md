89c87a7313dc5645f0be5e7ae290db484e46d609
unknown <yapadubpadoo@gmail.com>
2018-02-02 00:18:58 +0700

    add docker file and settings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wu2018
+
+
+## Troubleshooting Docker for Windows
+If cannot download the image due to "client timeout" try
+
+https://github.com/docker/for-win/issues/611
---
+++ app/index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css"/>
+
+<section class="hero is-primary is-medium">
+  <div class="hero-body">
+    <div class="container">
+        <h1 class="title is-1">
+            Hello from the other side!
+        </h1>
+        <h2 class="subtitle">
+            WU2018
+        </h2>
+    </div>
+  </div>
+</section>
