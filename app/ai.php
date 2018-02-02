ed8fcd92fe42ef97a3ff479fbb2cd0528c9180d7
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-02-02 23:59:41 +0700

    Update structure of AI

--- app/ai.php
+++ app/ai.php
@@ -2,8 +2,45 @@
 
 class AI
 {
-    public static function process($data)
+    public static function process($text)
     {
-        return $data;
+        $result = [
+            'gender' => self::getGender($text),
+            'sentiment' => self::getSentiment($text),
+            'rude_words' => self::getRudeWords($text),
+            'languages' => self::getLanguages($text),
+        ];
+    }
+
+    /**
+     * @return string 'Male' or 'Female' or 'Unknown'
+     */
+    public static function getGender($text)
+    {
+        return 'Male';
+    }
+
+    /**
+     * @return string 'Positive' or 'Neutral' or 'Negative'
+     */
+    public static function getSentiment($text)
+    {
+        return 'Neutral';
+    }
+
+    /**
+     * @return array of all rude words in Thai
+     */
+    public static function getRudeWords($text)
+    {
+        return ['แสส'];
+    }
+
+    /**
+     * @return array of languages (TH, EN)
+     */
+    public static function getLanguages($text)
+    {
+        return ['TH'];
     }
 }
