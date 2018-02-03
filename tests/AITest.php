6719799c1cb74762ab334752d7832c0b6641e2fd
DESKTOP-7TNDKDM\TUM-PC <pk.prateep@gmail.com>
2018-02-03 17:06:56 +0700

    add test case female

--- tests/AITest.php
+++ tests/AITest.php
@@ -10,4 +10,11 @@ final class AITest extends TestCase
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
+
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีคะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
 }
