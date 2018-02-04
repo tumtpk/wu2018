1155c2ce1c9b8e66de96d7e748b96138e60a626f
DESKTOP-7TNDKDM\TUM-PC <pk.prateep@gmail.com>
2018-02-04 10:27:39 +0700

    add test case

--- tests/AITest.php
+++ tests/AITest.php
@@ -17,4 +17,68 @@ final class AITest extends TestCase
         $expected_result = 'Female';
         $this->assertEquals($expected_result, $result);
     }
+
+    public function testGender_MaleBeginWord(): void
+    {
+        $result = AI::getGender('ผมชื่อนาย');
+        $expected_result = 'Male';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testGender_FemaleBeginWord(): void
+    {
+        $result = AI::getGender('ฉันชื่อนาง');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testSentiment_Negative(): void
+    {
+        $result = AI::getSentiment('เธอเป็นคนที่ไม่น่ารัก');
+        $expected_result = 'Negative';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testSentiment_Positive(): void
+    {
+        $result = AI::getSentiment('เธอเป็นคนที่สมบูรณ์แบบมาก');
+        $expected_result = 'Positive';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testSentiment_Neutral(): void
+    {
+        $result = AI::getSentiment('สวัสดีครับ');
+        $expected_result = 'Neutral';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testRudeWords_True(): void
+    {
+        $result = AI::getRudeWords('มึงไม่น่ารักกู');
+        $expected_result = ['กู', 'มึง'];
+        $this->assertTrue(count(array_diff_key($result, $expected_result)) === 0);
+    }
+
+    public function testLanguages_English(): void
+    {
+        $result = AI::getLanguages('Hello');
+        $expected_result = ['EN'];
+        $this->assertTrue(count(array_diff_key($result, $expected_result)) === 0);
+    }
+
+    public function testLanguages_Thai(): void
+    {
+        $result = AI::getLanguages('สวัสดี');
+        $expected_result = ['TH'];
+        $this->assertTrue(count(array_diff_key($result, $expected_result)) === 0);
+    }
+
+    public function testLanguages_EnglishThai(): void
+    {
+        $result = AI::getLanguages('Hello สวัสดี');
+        $expected_result = ['EN', 'TH'];
+        $this->assertTrue(count(array_diff_key($result, $expected_result)) === 0);
+    }
+
 }
